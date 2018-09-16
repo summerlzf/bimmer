0a820e21058a589af1c8325c2967197f25bcd070
Jef <summerlzf123@126.com>
2018-09-16 18:45:46 +0800

    初始化

---
+++ src/main/java/com/kedu/bimmer/BimmerApplication.java
@@ -0,0 +1,12 @@
+package com.kedu.bimmer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BimmerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BimmerApplication.class, args);
+	}
+}
