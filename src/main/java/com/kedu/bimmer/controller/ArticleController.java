667cfd4dba968c695b20e04ede2e22d9faa7639e
Jef <summerlzf123@126.com>
2018-12-01 04:13:24 +0800

    文章详情页URL地址调整

--- src/main/java/com/kedu/bimmer/controller/ArticleController.java
+++ src/main/java/com/kedu/bimmer/controller/ArticleController.java
@@ -1,8 +1,12 @@
 package com.kedu.bimmer.controller;
 
+import com.kedu.bimmer.base.GUID;
+import com.kedu.bimmer.model.Article;
 import com.kedu.bimmer.service.ArticleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -14,8 +18,12 @@ public class ArticleController {
     @Autowired
     private ArticleService articleService;
 
-    @RequestMapping("/article")
-    public String article(String id) {
+    @RequestMapping("/article/{id}")
+    public String article(Model model, @PathVariable("id") String id) {
+        Article vo = GUID.isGUID(id) ? articleService.get(id) : null;
+        if (vo == null) {
+            return "redirect:/";
+        }
         return "article";
     }
 }
